291614dc52c12974ce10fac4f7d2ea0b0be4adb5
Andrew Jo <andrewjo@gmail.com>
2015-03-06 11:48:00 -0800

    Force HTTPS

--- index.php
+++ index.php
@@ -40,6 +40,13 @@ if ( !function_exists( 'version_compare' ) || version_compare( PHP_VERSION, '5.3
 	wfPHPVersionError( 'index.php' );
 }
 
+# Force HTTPS
+if($_SERVER["HTTPS"] != "on")
+{
+		header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+		exit();
+}
+
 require __DIR__ . '/includes/WebStart.php';
 
 $mediaWiki = new MediaWiki();
